d8ab7c834d1b84f808b7d5ab6734821cc8a1e8a9
pratheeraja <eprathee@gmail.com>
2016-06-14 19:39:36 +0530

    added some more

--- bs4/bs4.py
+++ bs4/bs4.py
@@ -1,7 +1,13 @@
 import requests, bs4
 from BeautifulSoup import *
+
 res = requests.get('http://www.cricbuzz.com/cricket-match/live-scores')
 res.raise_for_status()
-score_text = bs4.BeautifulSoup(res.text)
-score=score_text.select('span')
-print(type(score))
+soup = bs4.BeautifulSoup(res.text)
+mydivs = soup.findAll("div", { "class" : "cb-lv-scrs-col cb-font-12 text-black" })
+le=len(mydivs)
+result=""
+for l in range(0,le-1):
+    result += str(mydivs[l].getText())+'\n'
+print(result)
+
