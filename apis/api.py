9827b402fd8e35d3b533e1a37034fd9f3d295f36
pratheeraja <eprathee@gmail.com>
2016-06-18 20:03:18 +0530

    stop watch

---
+++ apis/api.py
@@ -0,0 +1,16 @@
+def main():
+    """This function handles the actual logic of this script."""
+
+    # Grab CPI/Inflation data.
+
+    # Grab API/game platform data.
+
+    # Figure out the current price of each platform.
+    # This will require looping through each game platform we received, and
+    # calculate the adjusted price based on the CPI data we also received.
+    # During this point, we should also validate our data so we do not skew
+    # our results.
+
+    # Generate a plot/bar graph for the adjusted price data.
+
+    # Generate a CSV file to save for the adjusted price data.
